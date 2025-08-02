43be184a11a38d8bc4a398c92a1eec9c11d48ac7
tm1bisa <tingcard9@gmail.com>
2025-08-02 14:23:14 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey : "AIzaSyCEnKYCY3xJRfgLXuB12E8XDf4IvLussjg" , 
+  authDomain : "tm-1-18994.firebaseapp.com" , 
+  projectId : "tm-1-18994" , 
+  penyimpananBucket : "tm-1-18994.firebasestorage.app" , 
+  messageSenderId : "322788242615" , 
+  appId : "1:322788242615:web:77f2006d7ce3942a1cc932" 
+};
